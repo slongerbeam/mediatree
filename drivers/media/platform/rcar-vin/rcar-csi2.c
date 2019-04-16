bf8f61dfaf09280a5712c3843dc481968e0dbf17
Steve Longerbeam <slongerbeam@gmail.com>
2019-04-16 16:20:13 -0700

    media: rcar-csi2: Fix field detection
    The RCAR Gen3 hardware manual doesn't mention that the "Word Count" field
    in the MIPI CSI-2 Frame Start packet is really the frame number. FS packets
    are short packets, and short packets do not have a WC but rather a 16-bit
    data field, and for Frame synchronization packets (FS/FE) the 16-bit data
    field contains a frame number.
    
    Here is a reprinting from the MIPI CSI-2 specification, version 2
    (section 9.1.2 Low Level Protocol Short Packet Format):
    
    "Figure 42 and Figure 43 show the Low Level Protocol Short Packet
    structures for the D-PHY and C-PHY physical layer options, respectively.
    For each option, the Short Packet structure matches the Packet Header of
    the corresponding Low Level Protocol Long Packet structure with the
    exception that the Packet Header Word Count (WC) field shall be replaced
    by the Short Packet Data Field...For Frame Synchronization Data Types
    the Short Packet Data Field shall be the frame number..."
    
    Also in section 9.8.1 Frame Synchronization Packets, the CSI-2 spec reads:
    
    "The behavior of the 16-bit frame number shall be as one of the following
    - Frame number is always zero – frame number is inoperative.
    - Frame number increments by 1 for every FS packet with the same Virtual
      Channel and is periodically reset to one e.g. 1, 2, 1, 2, 1, 2, 1, 2 or
      1, 2, 3, 4, 1, 2, 3, 4"
    
    So from the above, FLD_NUM in the FLD register matches against the
    frame number transmitted by the CSI-2 source in the Frame Start Packet,
    and goes as "1,2,1,2,1,2" or "1,2,3,4,...,1,2,3,4". If there is a match,
    the RCAR CSI-2 receiver declares the field as the even, e.g. the BOTTOM,
    field.
    
    NTSC transmits bottom/even field first, and PAL and HD transmit top/odd
    field first. So fields with a frame number 1 in the CSI-2 Frame Start
    Packet are bottom/even fields for NTSC, and frame number 2 are bottom/even
    fields for PAL/HD.
    
    But the above assumes the transmitting CSI-2 sensor is sending frame
    numbers in the sequence "1,2,1,2,...". But according to the CSI-2 spec,
    sensors are also allowed to send frame numbers that simply increment as
    in "1,2,3,4,...". To generalize to catch those cases, set FLD_DET_SEL
    to 0x01, which according to the RCAR Gen3 hardware manual means "the
    field is detected as the EVEN field when FLD_NUM[0] matches WC[0]".
    Then, the even/bottom fields have odd frame numbers for NTSC, and even
    frame numbers for PAL (this patch assumes HDMI sources will not
    implement .g_std(), so std will be 0 in that case).
    
    Finally, set the FLD register to non-zero value only if trnsmitter is
    sending ALTERNATE fields.
    
    Signed-off-by: Steve Longerbeam <slongerbeam@gmail.com>

--- drivers/media/platform/rcar-vin/rcar-csi2.c
+++ drivers/media/platform/rcar-vin/rcar-csi2.c
@@ -68,6 +68,7 @@ struct rcar_csi2;
 /* Field Detection Control */
 #define FLD_REG				0x1c
 #define FLD_FLD_NUM(n)			(((n) & 0xff) << 16)
+#define FLD_FLD_DET_SEL(n)		(((n) & 0x3) << 4)
 #define FLD_FLD_EN4			BIT(3)
 #define FLD_FLD_EN3			BIT(2)
 #define FLD_FLD_EN2			BIT(1)
@@ -475,7 +476,7 @@ static int rcsi2_calc_mbps(struct rcar_csi2 *priv, unsigned int bpp)
 static int rcsi2_start_receiver(struct rcar_csi2 *priv)
 {
 	const struct rcar_csi2_format *format;
-	u32 phycnt, vcdt = 0, vcdt2 = 0;
+	u32 phycnt, vcdt = 0, vcdt2 = 0, fld = 0;
 	unsigned int i;
 	int mbps, ret;
 
@@ -518,6 +519,27 @@ static int rcsi2_start_receiver(struct rcar_csi2 *priv)
 	rcsi2_write(priv, TREF_REG, TREF_TREF);
 	rcsi2_write(priv, PHTC_REG, 0);
 
+	/*
+	 * NTSC standard transmits even/bottom field first, then odd/top.
+	 * PAL standard and interlaced HD transmit odd/top field first,
+	 * then even/bottom.
+	 *
+	 * For CSI-2 sensors that transmit frame numbers (in the CSI-2
+	 * Frame Start packet) as "1,2,1,2,..." or "1,2,3,4,5,...", then:
+	 *
+	 * - for NTSC, the even/bottom fields have odd frame numbers.
+	 * - for PAL and HD, the even/bottom fields have even frame numbers.
+	 */
+	if (priv->mf.field == V4L2_FIELD_ALTERNATE) {
+		fld = FLD_FLD_DET_SEL(1) | FLD_FLD_EN4 | FLD_FLD_EN3 |
+			FLD_FLD_EN2 | FLD_FLD_EN;
+
+		if (priv->mf.height == 240)
+			fld |= FLD_FLD_NUM(1); /* NTSC */
+		else
+			fld |= FLD_FLD_NUM(0); /* PAL or HD */
+	}
+
 	/* Configure */
 	rcsi2_write(priv, VCDT_REG, vcdt);
 	if (vcdt2)
@@ -549,8 +571,7 @@ static int rcsi2_start_receiver(struct rcar_csi2 *priv)
 	rcsi2_write(priv, PHYCNT_REG, phycnt);
 	rcsi2_write(priv, LINKCNT_REG, LINKCNT_MONITOR_EN |
 		    LINKCNT_REG_MONI_PACT_EN | LINKCNT_ICLK_NONSTOP);
-	rcsi2_write(priv, FLD_REG, FLD_FLD_NUM(2) | FLD_FLD_EN4 |
-		    FLD_FLD_EN3 | FLD_FLD_EN2 | FLD_FLD_EN);
+	rcsi2_write(priv, FLD_REG, fld);
 	rcsi2_write(priv, PHYCNT_REG, phycnt | PHYCNT_SHUTDOWNZ);
 	rcsi2_write(priv, PHYCNT_REG, phycnt | PHYCNT_SHUTDOWNZ | PHYCNT_RSTZ);
 
